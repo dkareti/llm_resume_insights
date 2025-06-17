944833620daf412bb7e9101c1ab206a120a38ba7
Daniel Kareti <danielkareti@gmail.com>
2025-06-17 16:19:02 -0700

    For production deployment

--- app.py
+++ app.py
@@ -34,6 +34,6 @@ def index():
     return render_template("index.html", tagline=tagline)
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
